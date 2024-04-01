a745c836d9d04d0651378addbce891f14e1f6e45
Satyaki Ghosh <satyakighosh594@gmail.com>
2024-04-01 21:31:30 +0530

    Adding Exceptional Handling Mechanism

---
+++ Sample-SpringBoot-Application/src/main/java/com/practice/SampleSpringBootApplication/Exception/ResourceNotFoundException.java
@@ -0,0 +1,27 @@
+package com.practice.SampleSpringBootApplication.Exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
+public class ResourceNotFoundException extends RuntimeException 
+{
+	private String resourceName;
+	private String fieldName;
+	private int fieldValue;
+	
+	public ResourceNotFoundException(String resourceName, String fieldName, int fieldValue) {
+		super(String.format("%s not found with %s : '%s'",resourceName,fieldName,fieldValue));
+		this.resourceName = resourceName;
+		this.fieldName = fieldName;
+		this.fieldValue = fieldValue;
+	}
+	
+	@Override
+	public String toString() {
+		return "ResourceNotFoundException [resourceName=" + resourceName + ", fieldName=" + fieldName + ", fieldvalue="
+				+ fieldValue + "]";
+	}
+	
+	
+}
